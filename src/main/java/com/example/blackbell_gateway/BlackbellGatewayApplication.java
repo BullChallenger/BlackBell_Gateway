0d6eada5b4d3915af789fa4b3480f9c3942ec6a4
BullChallenger <am23am45@naver.com>
2023-06-18 19:15:31 +0900

    Init Commit

---
+++ src/main/java/com/example/blackbell_gateway/BlackbellGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.blackbell_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackbellGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackbellGatewayApplication.class, args);
+	}
+
+}
